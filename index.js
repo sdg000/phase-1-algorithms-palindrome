7a4c51b0d7d4e6b22d695d5d4f5efffc88d60fe1
Seth David Gyimah <esec26@gmail.com>
2022-06-22 17:18:28 +0000

    palindrome

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  word = word.toLowerCase()
+  let newWord = word.toLowerCase().split('').reverse().join('')
+
+  if (word === newWord){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  1. function with word as param
+  2. change word to lower case
+  3. copy word to another variable
+  4  split wordCopy to array, reverse wordCopy, and rejoin wordCopy
+  5. compare word to wordCopy using if statement
 */
 
 /*
   Add written explanation of your solution here
+
+  change word to lower case for comparison
+  make a copy of word
+  change word to array, reverse and compare to oldWord 
 */
 
 // You can run `node index.js` to view these console logs
